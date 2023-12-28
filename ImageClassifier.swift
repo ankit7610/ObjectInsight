295087b410e44ffc7917aa043a1a28b8dd23ef65
ankit7610 <129362240+ankit7610@users.noreply.github.com>
2023-12-28 11:17:09 +0530

    Add files via upload

---
+++ ImageClassifier.swift
@@ -0,0 +1,26 @@
+//
+//  ImageClassifier.swift
+//  MLModel
+//
+//  Created by Ankit Kaushik on 26/12/23.
+//
+
+import Foundation
+import SwiftUI
+
+class ImageClassifier: ObservableObject {
+    
+    @Published private var classifier = Classifier()
+    
+    var imageClass: String? {
+        classifier.results
+    }
+        
+    // MARK: Intent(s)
+    func detect(uiImage: UIImage) {
+        guard let ciImage = CIImage (image: uiImage) else { return }
+        classifier.detect(ciImage: ciImage)
+        
+    }
+        
+}
